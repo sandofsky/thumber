a8528e135bb10f6e6a1569ef952632ce3ffb288c
Ben Sandofsky <sandofsky@gmail.com>
2009-04-15 16:43:35 -0700

    Initial checkin.

---
+++ main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Thumber
+//
+//  Created by Ben Sandofsky on 4/15/09.
+//  Copyright Yellowpages.com 2009. All rights reserved.
+//
+
+#import <MacRuby/MacRuby.h>
+
+int main(int argc, char *argv[])
+{
+    return macruby_main("rb_main.rb", argc, argv);
+}
